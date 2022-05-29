337248f8792a121945103541fe1e4e1007455587
lcmongwe <lcmongwe@gmail.com>
2022-05-29 19:30:05 +0300

    save location method created

--- portapp/models.py
+++ portapp/models.py
@@ -7,6 +7,9 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
+    def save_location(self):
+        self.save()
+
 class Category(models.Model):
     name = models.CharField(max_length = 30)
     def __str__(self):
